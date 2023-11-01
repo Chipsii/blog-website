095cfdf458dd71f5239322f798874f788921d0ed
Hasib <hasibfire24@gmail.com>
2023-11-01 18:00:43 +0600

    start hero component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function HomePage () {
+    return (
+        <Fragment>
+            <Hero />
+            <FeaturedProject />
+        </Fragment>
+    );
+}
+
+export default HomePage;
---
+++ pages/posts/[slug].js
@@ -0,0 +1,9 @@
+function PostDetailsPage () {
+    return (
+        <div>
+            <h1>Post Details Page</h1>
+        </div>
+    );
+}
+
+export default PostDetailsPage;
